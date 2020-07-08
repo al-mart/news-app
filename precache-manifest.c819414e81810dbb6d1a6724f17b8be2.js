e0ecfa4e204128d8e852dea02c809bdc1b4a5bae
Al Mart <aleksandr.martirosyan@gmail.com>
2020-07-08 18:41:14 +0400

    Updates

---
+++ precache-manifest.c819414e81810dbb6d1a6724f17b8be2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61fc9326d12b2b78059104be5b513e3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60f1a0489cbb4fb37911",
+    "url": "/static/css/main.ac7df153.chunk.css"
+  },
+  {
+    "revision": "c59c567f7137fe3cdf11",
+    "url": "/static/js/2.9a51e685.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a51e685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f1a0489cbb4fb37911",
+    "url": "/static/js/main.5dd018c1.chunk.js"
+  },
+  {
+    "revision": "f76bbb8b006909bb2d5d",
+    "url": "/static/js/runtime-main.b98ba16f.js"
+  },
+  {
+    "revision": "bd7766ade850d4c9c7fbf7b4b4c4adc0",
+    "url": "/static/media/fakeNews.bd7766ad.png"
+  }
+]);
